420027110809f60c8d07d35efafa6660ac3bae37
Purushotham Eluri <peluri@gmail.com>
2012-12-11 23:32:52 -0700

    Responsive Web CSS changes

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,22 @@
 /**
  * @author eluri
  */
+
+$(document).ready(function ()
+		{  
+			alert(12)
+			setTimeout(function ()
+			{  
+			    var w1 = $(window).width();  
+			    $('meta[name="viewport"]').remove();//remove previous metatag (no reason just so we keep it clean)  
+			    var px = 480;//target width to achieve  
+			    var x = 320/w1;// calculate ratio (320 is the width set by viewport on head->meta,  
+			    var out = Math.floor(px*x);//outcome..  
+			    // var out = Math.floor( (32/ w1) * 480 )// alternative shortcode remove the 3 lines above <img src="http://www.jqui.net/wp-includes/images/smilies/icon_smile.gif" alt=":)" class="wp-smiley">   
+			    $('<meta name="viewport" id="the_vp_special" content="width=320,initial-scale=1.0,target-densityDPI='+out+'dpi">').appendTo('head');//insert the new meta tag on the head   
+			},250);  
+		}); 
+		
 function playAudio()
 {
 	alert(1)
